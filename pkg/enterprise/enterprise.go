4a754f43c4b8cca65c5c165e761e4ea225dd5fd6
Brett Kuhlman <kuhlman-labs@github.com>
2023-04-06 10:04:08 -0400

    Adding Enterprise Membership report

--- pkg/enterprise/enterprise.go
+++ pkg/enterprise/enterprise.go
@@ -2,7 +2,9 @@ package enterprise
 
 import (
 	"context"
+	"encoding/csv"
 	"log"
+	"os"
 	"time"
 
 	"github.com/kuhlman-labs/octo-reports/internal/client"
@@ -12,6 +14,12 @@ import (
 type Org struct {
 	Login githubv4.String
 	ID    githubv4.String
+}
+
+type Member struct {
+	Id    string
+	Name  string
+	Login string
 }
 
 func GetEnterpriseOrgs(enterpriseSlug, token string) ([]*Org, error) {
@@ -63,4 +71,89 @@ func GetEnterpriseOrgs(enterpriseSlug, token string) ([]*Org, error) {
 	log.Printf("Fetched all orgs in %s", time.Since(start))
 
 	return allOrgs, nil
+}
+
+func getEnterpriseMembers(enterpriseSlug, token string) ([]*Member, error) {
+	client := client.NewV4Client(token)
+
+	variables := map[string]interface{}{
+		"enterpriseSlug": githubv4.String(enterpriseSlug),
+		"cursor":         (*githubv4.String)(nil),
+	}
+
+	var query struct {
+		Enterprise struct {
+			Members struct {
+				PageInfo struct {
+					EndCursor   githubv4.String
+					HasNextPage bool
+				}
+				Nodes []struct {
+					EnterpriseUserAccount struct {
+						Id    string
+						Login string
+						User  struct {
+							Name string
+						}
+					} `graphql:"... on EnterpriseUserAccount"`
+				}
+			} `graphql:"members(first: 100, after: $cursor)"`
+		} `graphql:"enterprise(slug: $enterpriseSlug)"`
+	}
+
+	allMembers := []*Member{}
+	start := time.Now()
+	log.Printf("Fetching all members for %s", enterpriseSlug)
+	for {
+		err := client.Query(context.Background(), &query, variables)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, member := range query.Enterprise.Members.Nodes {
+			allMembers = append(allMembers, &Member{
+				Login: member.EnterpriseUserAccount.Login,
+				Name:  member.EnterpriseUserAccount.User.Name,
+				Id:    member.EnterpriseUserAccount.Id,
+			})
+		}
+
+		if !query.Enterprise.Members.PageInfo.HasNextPage {
+			break
+		}
+		variables["cursor"] = githubv4.NewString(query.Enterprise.Members.PageInfo.EndCursor)
+	}
+
+	log.Printf("Found %d members in the %s Enterprise", len(allMembers), enterpriseSlug)
+	log.Printf("Fetched all members in %s", time.Since(start))
+
+	return allMembers, nil
+}
+
+func GenerateEnterpriseMembershipReport(enterpriseSlug, token string) error {
+
+	file, err := os.Create("enterprise-membership-report.csv")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write([]string{"Login", "Name", "Id"})
+
+	allMembers, err := getEnterpriseMembers(enterpriseSlug, token)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, member := range allMembers {
+		writer.Write([]string{string(member.Login), string(member.Name), string(member.Id)})
+	}
+
+	log.Printf("Wrote %d records to enterprise-membership-report.csv", len(allMembers))
+
+	return nil
 }
