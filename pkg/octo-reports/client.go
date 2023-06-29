699e94474fb457b2ffbae81a54a44c2fbe7257cd
Brett Kuhlman <kuhlman-labs@github.com>
2023-06-29 10:36:21 -0400

    Adding explixit Proxy config

--- pkg/octo-reports/client.go
+++ pkg/octo-reports/client.go
@@ -2,6 +2,7 @@ package octoreports
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/google/go-github/v50/github"
 	"github.com/shurcooL/githubv4"
@@ -18,10 +19,24 @@ type RateLimit struct {
 }
 
 func NewV4Client(url, token string) *githubv4.Client {
+
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+	}
+
+	ctx := context.Background()
+
+	httpClient := &http.Client{
+		Transport: transport,
+	}
+
+	ctx = context.WithValue(ctx, oauth2.HTTPClient, httpClient)
+
 	src := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: token},
 	)
-	httpClient := oauth2.NewClient(context.Background(), src)
+
+	httpClient = oauth2.NewClient(ctx, src)
 
 	if url != "https://api.github.com/graphql" {
 		client := githubv4.NewEnterpriseClient(url, httpClient)
